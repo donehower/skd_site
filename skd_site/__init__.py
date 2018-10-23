7c3284298685c7e829fd50c8ed3d9f9601afaaa8
Sarah Donehower <sarahdonehower@gmail.com>
2018-10-23 15:19:38 -0700

    Create app init.py

---
+++ skd_site/__init__.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_pyfile('config.py')
+
+# --------------------------------- #
+#         Database Set-up           #
+# --------------------------------- #
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+Migrate(app, db)
+
+# --------------------------------- #
+#             Blue Prints           #
+# --------------------------------- #
+from skd_site.core.views import core
+from skd_site.weather.views import skd
+
+app.register_blueprint(core)
+app.register_blueprint(skd)
