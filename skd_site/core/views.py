538059f5375d33f2f999acd0f2a05912bc729232
Sarah Donehower <sarahdonehower@gmail.com>
2018-11-12 11:43:13 -0800

    Added views for senate page

--- skd_site/core/views.py
+++ skd_site/core/views.py
@@ -1,4 +1,5 @@
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, jsonify
+from skd_site import mongo
 
 
 core = Blueprint('core', __name__)
@@ -32,3 +33,17 @@ def projects():
 def campaign_notebook():
 
     return render_template('campaign_notebook.html')
+
+
+@core.route('/d3_senate', methods=['GET', 'POST'])
+def d3_senate():
+
+    return render_template('d3_senate.html')
+
+
+@core.route('/get_senate_data', methods=['GET', 'POST'])
+def get_senate_data():
+
+    data = mongo.db.senate_data.find()
+
+    return jsonify(data)
